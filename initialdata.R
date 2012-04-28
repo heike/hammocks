194d6d3f5a1db97da42514deeab8f688373500fd
mariev <mariev@iastate.edu>
2012-04-28 10:13:15 -0500

    df4  and df5 organize qualtrics csv for use in R

--- initialdata.R
+++ initialdata.R
@@ -95,6 +95,99 @@ vertical, horizontal, circos 7
 #  monitorH: monitor height (px)
 ##########################################
 
+
+
+
+################## helper function
+getAnswers <- function(df4,i, temp1, temp2,timing, qAs, qBs = NA, qCs = NA){
+	naming <- "Response"
+	timenames <- paste("Time", LETTERS[1:length(timing)], sep = "")
+	clicknames <- paste("Clicks", LETTERS[1:length(timing)], sep ="")
+	for(j in 1:length(timing)){
+		df4[df4$ID == i, timenames[j]] <- round(as.numeric(as.character(subset(temp2, select = 
+			paste("Q", timing[j],"_3", sep = ""))[[1]])), 2)
+		df4[df4$ID == i, clicknames[j]] <- as.numeric(as.character(subset(temp2, select = paste("Q", 
+			timing[j], "_4", sep = ""))[[1]]))
+	}
+	## case1: respondant did not change order for A
+	## case2: all other responses
+	namesA <- paste(naming, "A", 1:3, sep = "")
+	for(j in 1:3){
+		if(subset(temp2, select = paste("Q", qAs[j], "_1", sep = ""))[[1]] == 1 & subset(temp2, select = 
+			paste("Q", qAs[j], "_2", sep = ""))[[1]] == 2 & subset(temp2, select = paste("Q", qAs[j], "_3", 
+			sep = ""))[[1]] == 3 & subset(temp2, select = paste("Q", qAs[j], "_4", sep = ""))[[1]] == 4){
+			df4[df4$ID == i, namesA[j]] <- "No Response"
+		} else {
+			df4[df4$ID == i, namesA[j]] <- paste(subset(temp2, select = 
+				paste("Q", qAs[j], "_1", 
+				sep = ""))[[1]],subset(temp2, select = paste("Q", qAs[j], "_2", sep = ""))[[1]], 
+				subset(temp2, select = 	paste("Q", qAs[j], "_3", sep = ""))[[1]], sep = "_")
+		}
+	}
+	namesB <- paste(naming, "B", 1:3, sep = "")
+
+	for(j in 1:3){
+		if(j == 2 && ((subset(temp2, select = paste("Q", qBs[j], "_1", sep = ""))[[1]] == 1 & 
+			subset(temp2, select = 
+			paste("Q", qBs[j], "_2", sep = ""))[[1]] == 2 & subset(temp2, select = paste("Q", qBs[j], "_3", 
+			sep = ""))[[1]] == 3 & subset(temp2, select = paste("Q", qBs[j], "_4", sep = ""))[[1]] == 4) |
+			(subset(temp2, select = paste("Q", qBs[j], "_1", sep = ""))[[1]] == "" & 
+			subset(temp2, select = 
+			paste("Q", qBs[j], "_2", sep = ""))[[1]] == "" & subset(temp2, select = paste("Q", qBs[j], "_3", 
+			sep = ""))[[1]] == "" & subset(temp2, select = paste("Q", qBs[j], "_4", sep = ""))[[1]] == ""))){
+		    df4[df4$ID == i, namesB[j]] <- "No Response"
+		} else if(j == 2){
+
+			df4[df4$ID == i, namesB[j]] <- paste(subset(temp2, select = 
+				paste("Q", qBs[j], "_1", 
+				sep = ""))[[1]],subset(temp2, select = paste("Q", qBs[j], "_2", sep = ""))[[1]], 
+				subset(temp2, select = 	paste("Q", qBs[j], "_3", sep = ""))[[1]], sep = "_")
+		} else {
+
+			if(j == 1){
+				
+				df4[df4$ID == i, namesB[j]] <- subset(temp2, select = qBs[j])[[1]]
+			} else if (j == 3){
+
+				df4[df4$ID == i, namesB[j]] <- paste(subset(temp2, select = 
+				paste("Q", qBs[j], "_1", 
+				sep = ""))[[1]],subset(temp2, select = paste("Q", qBs[j], "_2", sep = ""))[[1]], 
+				subset(temp2, select = 	paste("Q", qBs[j], "_3", sep = ""))[[1]], sep = "_")
+
+			}
+		}
+	}
+
+	namesC <- paste(naming, "C", 1:3, sep ="")
+	for (j in 1:3){
+		if(j == 3){
+			df4[df4$ID == i, namesC[j]] <- subset(temp2, select = paste("Q", qCs[j], sep = ""))[[1]]
+		} else{
+			if((subset(temp2, select = paste("Q", qCs[j], "_1", sep = ""))[[1]] == 1 & 
+			subset(temp2, select = 
+			paste("Q", qCs[j], "_2", sep = ""))[[1]] == 2 & subset(temp2, select = paste("Q", qCs[j], "_3", 
+			sep = ""))[[1]] == 3 & subset(temp2, select = paste("Q", qCs[j], "_4", sep = ""))[[1]] == 4) |
+			(subset(temp2, select = paste("Q", qCs[j], "_1", sep = ""))[[1]] == "" & 
+			subset(temp2, select = 
+			paste("Q", qCs[j], "_2", sep = ""))[[1]] == "" & subset(temp2, select = paste("Q", qCs[j], "_3", 
+			sep = ""))[[1]] == "" & subset(temp2, select = paste("Q", qCs[j], "_4", sep = ""))[[1]] == "")){
+				df4[df4$ID == i, namesC[j]] <- "No Response"
+			} else{
+				df4[df4$ID == i, namesC[j]] <- paste(subset(temp2, select = 
+				paste("Q", qCs[j], "_1", 
+				sep = ""))[[1]],subset(temp2, select = paste("Q", qCs[j], "_2", sep = ""))[[1]], 
+				subset(temp2, select = 	paste("Q", qCs[j], "_3", sep = ""))[[1]], sep = "_")
+
+			}
+		}
+	}
+
+	return(df4)
+}
+
+
+########### end helper function
+
 df4 <- data.frame(ID = unique(df2[,1]), TypeA = NA, ResponseA1 = NA, ResponseA2 = NA, 
 		ResponseA3 = NA, TimeA = NA, ClicksA = NA, TypeB = NA, ResponseB1 = NA, ResponseB2 = NA,
 		ResponseB3 = NA, TimeB = NA, ClicksB = NA, TypeC = NA, ResponseC1 = NA, ResponseC2 = NA,
@@ -114,6 +207,8 @@ for(i in df4$ID){
 	df4[df4$ID == i,]$monitorW <- strsplit(temp, "x")[[1]][1]
 	df4[df4$ID == i,]$monitorH <- strsplit(temp, "x")[[1]][2]
 	df4[df4$ID == i,]$Browser <- as.character(df2[df2[,1] == i,]$Q2_1_TEXT)
+	df4[df4$ID == i,]$Preference <- as.character(df2[df2[,1] == i, ]$Q89)
+	df4[df4$ID == i,]$Why <- as.character(df2[df2[,1] == i, ]$Q88)
 }
 
 # turn the block groupings into eye-readable names
@@ -137,58 +232,21 @@ for(i in df4$ID){
 	## questions: 99,3, 33, 34,35 in qualtrics 
 	## in raw csv: 
 	if(temp1$TypeA == "hammock" & temp1$TypeB == "bar" & temp1$TypeC == "circos"){
-		df4 <- getAnswers(df4 = df4,i = i, temp1 = temp1, temp2 = temp2, timing = 99, qAs = c(33,34,35),
-					qBs = c(51,52,53))
+		df4 <- getAnswers(df4 = df4,i = i, temp1 = temp1, temp2 = temp2, timing = c(99,100,101), qAs = c(33,34,35),	qBs = c(51,52,53), qCs = c(71,69,72))
+	} else if(temp1$TypeA == "circos" & temp1$TypeB == "hammock" & temp1$TypeC == "bar"){
+		df4 <- getAnswers(df4 = df4, i = i, temp1 = temp1, temp2 = temp2, timing = c(90,92,91), qAs = c(16,24,25), qBs = c(42,43,44), qCs = c(60,61,62))
+	} else if(temp1$TypeA == "circos" & temp1$TypeB == "bar" & temp1$TypeC == "hammock"){
+		df4 <- getAnswers(df4 = df4, i = i, temp1 = temp1, temp2 = temp2, timing = c(93:95), qAs = c(27:29),
+				qBs = c(45:47), qCs = c(63:65))
+	} else if(temp1$TypeA == "hammock" & temp1$TypeB == "circos" & temp1$TypeC == "bar"){
+		df4 <- getAnswers(df4 = df4, i = i, temp1 = temp1, temp2 = temp2, timing = c(96:98), qAs = c(30:32),
+		qBs = c(48:50), qCs = c(70,67,66))
+	}else if(temp1$TypeA == "bar" & temp1$TypeB == "circos" & temp1$TypeC == "hammock"){
+		df4 <- getAnswers(df4 = df4, i = i, temp1 = temp1, temp2 = temp2, timing = c(102:104), qAs = c(36:38),
+				qBs = c(54:56), qCs = c(73,68,74))
+	}else if(temp1$TypeA == "bar" & temp1$TypeB == "hammock" & temp1$TypeC == "circos"){
+		print("start")
+		df4 <- getAnswers(df4 = df4, i = i, temp1 = temp1, temp2 = temp2, timing = c(105:107), qAs =c(39:41),
+				qBs = c(57:59), qCs = c(75:77))
 	}
-}
-
-getAnswers <- function(df4,i, temp1, temp2,timing, qAs, qBs = NA, qCs = NA){
-	naming <- "Response"
-	
-	df4[df4$ID == i,]$TimeA <- as.numeric(as.character(subset(temp2, select = 
-		paste("Q", timing,"_3", sep = ""))[[1]]))
-	df4[df4$ID == i,]$ClicksA <- as.numeric(as.character(subset(temp2, select = paste("Q", 
-		timing, "_4", sep = ""))[[1]]))
-	## case1: respondant did not change order for A
-	## case2: all other responses
-	
-	namesA <- paste(naming, "A", 1:3, sep = "")
-	for(j in 1:3){
-		if(subset(temp2, select = paste("Q", qAs[j], "_1", sep = ""))[[1]] == 1 & subset(temp2, select = 
-			paste("Q", qAs[j], "_2", sep = ""))[[1]] == 2 & subset(temp2, select = paste("Q", qAs[j], "_3", 
-			sep = ""))[[1]] == 4 & subset(temp2, select = paste("Q", qAs[j], "_4", sep = ""))[[1]] == 4){
-			df4[df4$ID == i, namesA[j]] <- "No Response"
-		} else {
-			df4[df4$ID == i, namesA[j]] <- paste(subset(temp2, select = 
-				paste("Q", qAs[j], "_1", 
-				sep = ""))[[1]],subset(temp2, select = paste("Q", qAs[j], "_2", sep = ""))[[1]], 
-				subset(temp2, select = 	paste("Q", qAs[j], "_3", sep = ""))[[1]], sep = "_")
-		}
-	}
-	namesB <- paste(naming, "B", 1:3, sep = "")
-	for(j in 1:3){
-		if(j == 2 & subset(temp2, select = paste("Q", qBs[j], "_1", sep = ""))[[1]] == 1 & 
-			subset(temp2, select = 
-			paste("Q", qBs[j], "_2", sep = ""))[[1]] == 2 & subset(temp2, select = paste("Q", qBs[j], "_3", 
-			sep = ""))[[1]] == 4 & subset(temp2, select = paste("Q", qBs[j], "_4", sep = ""))[[1]] == 4){
-			df4[df4$ID == i, namesB[j]] <- "No Response"
-		}
-		 # else if(j == 2){
-			# df4[df4$ID == i, namesB[j]] <- paste(subset(temp2, select = 
-				# paste("Q", qBs[j], "_1", 
-				# sep = ""))[[1]],subset(temp2, select = paste("Q", qBs[j], "_2", sep = ""))[[1]], 
-				# subset(temp2, select = 	paste("Q", qBs[j], "_3", sep = ""))[[1]], sep = "_")
-		# } else {
-			# if(j == 1){
-				# df4[df4$ID == i, namesB[j]] <- subset(temp2, select = paste("Q", qBs[j], sep = ""))[[1]]
-			# } else if (j == 3){
-				# df4[df4$ID == i, namesB[j]] <- paste(subset(temp2, select = 
-				# paste("Q", qBs[j], "_1", 
-				# sep = ""))[[1]],subset(temp2, select = paste("Q", qBs[j], "_2", sep = ""))[[1]], 
-				# subset(temp2, select = 	paste("Q", qBs[j], "_3", sep = ""))[[1]], sep = "_")
-
-			# }
-		# }
-	}
-	return(df4)
 }
