e830f456bae0f58f5ade1405f97da81443592d22
Heike Hofmann <hofmann@iastate.edu>
2012-04-29 07:15:52 -0500

    partially graded responses

--- plotting_hh.R
+++ plotting_hh.R
@@ -16,3 +16,26 @@ qplot(as.numeric(qid)+(as.numeric(plottype)-2)/15, mean*100,
         geom_errorbar(aes(x=as.numeric(qid)+(as.numeric(plottype)-2)/15, 
                            ymin=pmax(100*(mean-1.96*sd/sqrt(n)), 0), 
                            ymax=pmin(100*(mean+1.96*sd/sqrt(n)), 100)))
+
+######################
+frame2 <- read.csv("gradedresponses2.csv")
+
+frame <- ddply(frame2, .(id, qid, dataset, plottype), summarize,
+               n=length(response),
+               correct=sum(correct))
+frame$qid <- factor(frame$qid)
+correct.mod <- glm(cbind(correct, n-correct)~dataset/qid/plottype-1, data=frame, family=binomial())
+summary(correct.mod)  
+
+
+res <- ddply(frame, .(dataset, qid, plottype), summarize,
+             n=length(qid),
+             mean=mean(correct/n),
+             sd=sd(correct/n))
+
+qplot(as.numeric(qid)+(as.numeric(plottype)-2)/15, mean*100, 
+      data=res, facets=.~dataset, colour=plottype) + scale_x_continuous(breaks=c(1:3)) + xlab("Question") + ylab("Percentage correct") + 
+        geom_errorbar(aes(x=as.numeric(qid)+(as.numeric(plottype)-2)/15, 
+                          ymin=pmax(100*(mean-1.96*sd/sqrt(n)), 0), 
+                          ymax=pmin(100*(mean+1.96*sd/sqrt(n)), 100)))
+
