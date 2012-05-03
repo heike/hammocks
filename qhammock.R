efb723e249a0890a0f7e372fedcf1c8544d3394b
mariev <mariev@iastate.edu>
2012-05-03 13:33:26 -0500

    lines and arg handling added

--- qhammock.R
+++ qhammock.R
@@ -1,35 +1,50 @@
 require(cranvas)
 require(qtpaint)
 require(qtbase)
+require(ddply)
 
-## todo specific args internally. 
-qhammock <- function(xlim, ylim, xat, yat, limits,x,  width, heights){
+## todo correct the lines for connections.
+
+qhammock <- function(x, variables, freq, xat = NULL, yat = NULL, width, pal = rainbow(n = 10)){
+	
+################# error handling
+	if(length(variables) != 2){
+		stop("qhammock can only handle 2 variables at this time!")
+	}
 
 ##################
 ################## helper functions
 ##################
-	getxlim <- function(){
-		return(xlim)
+	getxlim <- function(vals){
+		return(c(-1, 1) * diff(vals) * 2 * width + vals)
 	}
 	
 	getylim <- function(){
-		return(ylim)
+		return(c(0, 1.1 * sum(x[freq])))
 	}
 	
 	getxat <- function(){
-		return(xat)
+		if(is.null(xat)){
+			return(1:length(variables))
+		} else {
+			return(xat)
+		}
 	}
 	
 	getyat <- function(){
-		return(yat)
+		if(is.null(yat)){
+			return(sum(x[freq]))
+		} else {
+			return(yat)
+		}
 	}
 	
-	getlimits <- function(){
-		return(qrect(matrix(limits, 2)))
+	getlimits <- function(varx, vary){
+		return(qrect(matrix(c(varx, vary), 2)))
 	}
 	
 	getx <- function(){
-		return(x)
+		return(1:length(variables))
 	}
 	
 	getrectwidths <- function(){
@@ -37,24 +52,45 @@ qhammock <- function(xlim, ylim, xat, yat, limits,x,  width, heights){
 	}
 	
 	getrectheights <- function(){
-		return(heights)
+		return(list(V1 = ddply(.data = x, .variables = variables[1], .fun = function(x){sum(x[freq])})$V1,
+						  V2 = ddply(.data = x, .variables = variables[2], .fun = function(x){sum(x[freq])})$V1))
 	}
 ################ end helper functions
-	
+
 	
 ############### transform input variables for cranvas
-	l <- getlimits()
+	
 	h <- getrectheights()
-	y_bottom <- c(0, cumsum(h[[1]])[-length(h[[1]])])
-	y_top <- cumsum(h[[1]])
+	xticks <- getxat()
+	xlim <- getxlim(xticks)
+	yticks <- getyat()
+	ylim <- getylim()
+	l <- getlimits(xlim, ylim)
+	
+	## for the y position of bars
+	y_bottom <- c(0,cumsum(h$V1)[-length(h$V1)], 0, cumsum(h$V2)[-length(h$V2)])
+	y_top <- c(cumsum(h$V1), cumsum(h$V2))
+
+	
+	## for the x position of bars
 	widths <- c(-1, 1) * getrectwidths()
 	rectx <- sapply(getx(), FUN = function(x){
 		   x + widths})
-	
 	x_left <- c(rep(rectx[1,1], length(h[[1]]))  ,rep(rectx[1,2], length(h[[2]])) )
 	x_right <- c(rep(rectx[2,1], length(h[[1]])),	rep(rectx[2,2], length(h[[2]])))
 	
-
+	
+	## for the x position of lines
+	xlines <- rep(c(rectx[2,1], rectx[1,2], NA), length(h$V1) * length(h$V2))
+	
+	## for the y position of lines
+	ylines_V1 <- rep(mapply(y = 1:length(h$V1), FUN = function(y){max(0, cumsum(h$V1)[y - 1]) + (h$V1[y] / 2)}),  length(h$V2))
+	temp <- ddply(.data = x, .variables = variables, .fun = function(x){sum(x[freq])})
+	temp <- temp[order(temp[variables[2]]),]
+	ylines_V2 <- mapply(y = 1:length(temp$V1), FUN = function(y){max(0, cumsum(temp$V1)[y - 1]) + (temp$V1[y] / 2)})
+	ylines <- as.vector(t(data.frame(v1 = ylines_V1, v2 = ylines_V2, NA)))
+	
+print(ylines)
 ############## draw the cranvas elements
 	scene <- qscene()
     layer.root <- qlayer(scene)
@@ -62,21 +98,23 @@ qhammock <- function(xlim, ylim, xat, yat, limits,x,  width, heights){
 
 	layer.main <- qlayer(paintFun = function(layer, painter){
 						 qdrawLine(painter,
-								   x = c(1.1,1.9, NA),
-								   y = c(745, 61, NA),
+x = xlines,
+								   y = ylines, 
+#								   x = c(1.1,1.9,NA),
+#								   y = c(745, 61, NA),
 								   stroke = "grey60")
 						 qdrawRect(painter, 
 								   xleft = x_left ,
 								   xright = x_right,
-								   ybottom = c(0,1490, 0, 325, 610, 1316),
-								   ytop = c(1490,2201,325, 610, 1316, 2201  ),
-								   fill = rainbow(n = 6))
+								   ybottom = y_bottom,
+								   ytop = y_top,
+								   fill = pal)
 						 } ,  
 								   limits = l)
 	
 	layer.brush <- qlayer(paintFun = function(layer, painter){
 						  }, limits = l)
-	layer.root[1, 1] <- qgrid(xat = getxat(), yat = getyat(), xlim = getxlim(), ylim = getylim(), limits = l, minor = "")	
+	layer.root[1, 1] <- qgrid(xat = xticks, yat = getyat(), xlim = xlim, ylim = getylim(), limits = l, minor = "")	
     layer.root[1, 1] <- layer.main
 #layer.root[1, 1] <- layer.brush
     view <- qplotView(scene = scene)
@@ -94,5 +132,5 @@ qhammock <- function(xlim, ylim, xat, yat, limits,x,  width, heights){
 #2   2nd 285
 #3   3rd 706
 #4  Crew 885
-qhammock(xat = c(1,2), yat = seq(0, 2000, 500), xlim = c(0, 3), ylim = c(0, 2225), limits = c(0,3,-5,2225),
-	x = 1:2, width = .1, heights = list(V1 = c(1490, 711), V2 = c(325, 285, 706, 885)))
+titanic <- data.frame(Titanic)
+qhammock(x = titanic, variables = c("Survived", "Class"), freq = "Freq", width = .1, pal = rainbow(n = 6))
