296e3f93bec385a14da9dbbdc9a71083b82d80c3
mariev <mariev@coconut>
2012-09-24 14:01:12 -0600

    updated pics

--- common_angles_study/image_code.r
+++ common_angles_study/image_code.r
@@ -1,11 +1,12 @@
 library(ggparallel)
 library(RColorBrewer)
-cols <- c(brewer.pal(5,"Blues")[-1], rev(brewer.pal(3, "Oranges")[-1]), brewer.pal(3, "Greens")[-1])
+cols <- c(brewer.pal(5,"Blues")[-1], rev(brewer.pal(3, "Oranges")[-1]), rev(brewer.pal(3, "Greens")[-1]))
+
 
 
 ggparallel(data = mtcars, vars = list('cyl', 'gear', 'carb', 'am' ), 
            method = "parset", text.angle = 0, order = 0) + theme_bw() + 
-          scale_y_continuous(breaks = seq(from =0, to = 30, by = 5)) + 
+          scale_y_continuous(breaks = seq(from =0, to = 30, by = 5)) +
           scale_colour_manual(values = c(brewer.pal('Dark2', n = 8), 
                             brewer.pal('Set2', n = 8)), guide = 'none') +
           scale_fill_manual(values = c(brewer.pal('Dark2', n = 8), brewer.pal('Set2', n = 8)),
@@ -41,8 +42,8 @@ ggsave(filename  = 'mtcars_ca.png')
 
 
 ggparallel(data = mtcars, vars = list('cyl', 'gear',  'carb', 'am' ), 
-           text.angle = 0, method = 'adj.angle', ratio = 2, order = 0) + theme_bw() + 
-             scale_y_continuous(breaks = seq(from =0, to = 30, by = 5)) + 
+           text.angle = 0, method = 'adj.angle', ratio = 2, order = 0) + theme_bw() +
+             scale_y_continuous(breaks = seq(from =0, to = 30, by = 5)) 
              scale_colour_manual(values = c(brewer.pal('Dark2', n = 8), 
                                             brewer.pal('Set2', n = 8)), guide = 'none') +
                                               scale_fill_manual(values = c(brewer.pal('Dark2', n = 8), brewer.pal('Set2', n = 8)),
@@ -55,35 +56,42 @@ ggsave(filename  = 'mtcars_aa.png')
 titanic <- data.frame(Titanic)
 
 
-ggparallel(names(titanic)[c(1,4,2,1)], order=c(0,1,1,0), titanic, weight="Freq", method = 'hammock', ratio = .3) +
+ggparallel(names(titanic)[c(1,4,2,1)], order=c(0,1,1,0), titanic, weight="Freq", method = 'hammock', 
+           ratio = .3) +
   scale_fill_manual(values=cols, guide="none") +
-  scale_colour_manual(values=cols, guide="none") + theme_bw()
+  scale_colour_manual(values=cols, guide="none") + theme_bw() +
+  scale_y_continuous(breaks = seq(from  = 0, to = 2000, by =250))
 
-ggsave(filename = 'titanic_hammock.png')
+ggsave(filename = 'titanic_hammock.png', width= 5, height = 5, units = 'in')
 
 
 ggparallel(names(titanic)[c(1,4,2,1)], order=c(0,1,1,0), titanic, weight="Freq") +
   scale_fill_manual(values=cols, guide="none") +
-  scale_colour_manual(values=cols, guide="none") + theme_bw()
+  scale_colour_manual(values=cols, guide="none") + theme_bw() +
+  scale_y_continuous(breaks = seq(from  = 0, to = 2000, by =250))
 
-ggsave(filename = 'titanic_ca.png')
+ggsave(filename = 'titanic_ca.png', width= 5, height = 5, units = 'in')
 
 
 ggparallel(names(titanic)[c(1,4,2,1)], order=c(0,1,1,0), alpha=0.6, method="adj.angle", ratio=.035, titanic, weight="Freq") +
   scale_fill_manual(values=cols, guide="none") +
-  scale_colour_manual(values=cols, guide="none") + theme_bw()
+  scale_colour_manual(values=cols, guide="none") + theme_bw() +
+  scale_y_continuous(breaks = seq(from  = 0, to = 2000, by =250))
 
-ggsave(filename = 'titanic_aa.png')
+ggsave(filename = 'titanic_aa.png', width= 5, height = 5, units = 'in')
 
-ggparallel(names(titanic)[c(1,4,2,1)], order=c(0,1,1,0), method="parset", titanic, weight="Freq") +
+ggparallel(names(titanic)[c(1,4,2,1)], order=c(0,1,1,0), asp.ratio =.6, method="parset", titanic, weight="Freq") +
   scale_fill_manual(values=cols, guide="none") +
-  scale_colour_manual(values=cols, guide="none") + theme_bw()
+  scale_colour_manual(values=cols, guide="none") + theme_bw() +
+  scale_y_continuous(breaks = seq(from  = 0, to = 2000, by =250))
 
-ggsave(filename = 'titanic_parset.png')
+ggsave(filename = 'titanic_parset.png', width= 5, height = 5, units = 'in')
 
 
 
 1st and female (145)
 Crew and survived( 212)
 Male and survived(367)
+
+
 
